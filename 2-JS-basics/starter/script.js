af1a9ca1ca2f8273c0de663682149b25b0e52e6f
co1injcasey <52921834+co1injcasey@users.noreply.github.com>
2019-07-15 09:21:42 -0400

    Add files via upload

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,111 @@
+
+/*
+var firstName = 'Colin';
+console.log(firstName);
+var lastName = 'Casey';
+console.log(lastName);
+var age = 23;
+var fullAge = true;
+console.log(fullAge);
+var job;
+console.log(job);
+job = 'Programmer';
+console.log(job);
+*/
+
+// var 3years = 3; //var names cant start with numbers
+// var _3years = 3; //var names can start with _ and then number, just no num first
+// all var must start with _ , $ , or letter 
+// var names should be made up 
+/*
+var firstName = 'colin';
+var age = 23;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Programmer';
+isMarried = false;
+
+console.log(firstName + 'is a ' + age + 
+            ' year old' + job + '. Is he married?' + isMarried);
+
+alert(firstName); 
+
+var lastName = prompt('what is his last name');
+console.log(firstName + ' ' + lastName);
+*/
+
+/* Basic OPERATORS */
+/*
+var now, ageColin, ageCasey
+now = 2019;
+ageColin = now - 23;
+ageCasey = now - 23;
+
+console.log(ageColin);
+
+console.log(now + 2);
+
+var colinOlder = ageColin > ageCasey;
+console.log(colinOlder);
+
+
+
+// typeof OPERATOR
+
+console.log(typeof colinOlder);
+console.log(typeof ageColin);
+
+*/
+/*
+var massColin = 200; //lbs
+var heightColin = 6; //ft
+
+var massAaron = 150; 
+var heightAaron = 6.3;
+
+
+var BMIColin = massColin / heightColin;
+var BMIAaron = massAaron / heightAaron;
+
+
+console.log(BMIColin);
+
+console.log(BMIAaron);
+
+var heavier = BMIAaron > BMIColin;
+console.log(heavier);
+*/
+/*
+var firstName = 'Colin';
+var marriedStatus ='single';
+
+if (marriedStatus === 'married') {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' is currently not married');
+}
+
+var isMarried = true; 
+if (isMarried) {
+    console.log(firstName + ' is married!');
+} else {
+    console.log(firstName + ' is currently not married');
+}
+
+*/ 
+
+var firstName = 'Colin';
+var civilStatues = 'single';
+
+if (civilStatus === 'married') {
+    console.log(firstName + ' is married!');
+}
+
+
+
+
+
+
+
